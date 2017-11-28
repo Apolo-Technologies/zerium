60e63038e94db38ed2948a25451cee1f57154b2c
hteijiz <hugo.teijiz@gmail.com>
2017-11-28 17:26:57 -0300

    ...

--- internal/jsre/deps/deps.go
+++ internal/jsre/deps/deps.go
@@ -17,5 +17,5 @@
 // Package deps contains the console JavaScript dependencies Go embedded.
 package deps
 
-//go:generate go-bindata -nometadata -pkg deps -o bindata.go bignumber.js webze.js
-//go:generate gofmt -w -s bindata.go
+go:generate go-bindata -nometadata -pkg deps -o bindata.go bignumber.js webze.js
+go:generate gofmt -w -s bindata.go
