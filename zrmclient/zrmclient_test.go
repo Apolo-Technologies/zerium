562345f0cb6339bd5fddf20c91e8c4c9dff510c9
hteijiz <hugo.teijiz@gmail.com>
2017-12-13 23:07:30 -0300

    ...

---
+++ zrmclient/zrmclient_test.go
@@ -0,0 +1,34 @@
+// Copyright 2017 The zerium Authors
+// This file is part of the zerium library.
+//
+// The zerium library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The zerium library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the zerium library. If not, see <http://www.gnu.org/licenses/>.
+
+package zrmclient
+
+import "github.com/apolo-technologies/zerium"
+
+// Verify that Client implements the zerium interfaces.
+var (
+	_ = zerium.ChainReader(&Client{})
+	_ = zerium.TransactionReader(&Client{})
+	_ = zerium.ChainStateReader(&Client{})
+	_ = zerium.ChainSyncReader(&Client{})
+	_ = zerium.ContractCaller(&Client{})
+	_ = zerium.GasEstimator(&Client{})
+	_ = zerium.GasPricer(&Client{})
+	_ = zerium.LogFilterer(&Client{})
+	_ = zerium.PendingStateReader(&Client{})
+	// _ = zerium.PendingStateEventer(&Client{})
+	_ = zerium.PendingContractCaller(&Client{})
+)
